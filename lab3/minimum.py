158253979f6b374b986d9b7bbbcbddefb9a151e2
Selim TIKIZ <selimtikiz@std.iyte.edu.tr>
2020-10-26 08:48:21 +0000

    minimum

---
+++ lab3/minimum.py
@@ -0,0 +1,11 @@
+sayi1= int(input("first number"))
+sayi2= int(input("second number"))
+sayi3= int(input("third number"))
+if(sayi1>=sayi2) and (sayi1>=sayi3):
+  biggest=sayi1
+ elif (sayi2>=sayi1) and (sayi2>=sayi3):
+    biggest= sayi2
+    else:
+    biggest = sayi3
+
+    print(biggest)
