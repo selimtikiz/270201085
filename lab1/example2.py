b33eba87311909dbcf21c62f27005a1f0e0a1d08
Selim TIKIZ <selimtikiz@std.iyte.edu.tr>
2020-11-15 11:47:34 +0000

    tutorial

---
+++ lab1/example2.py
@@ -0,0 +1,5 @@
+x = 10
+result = 0
+for num in range(1, x + 1):
+	result += num
+print(result)
--- lab3/season.py
+++ lab3/season.py
@@ -1,11 +1,23 @@
-month =(int(input("enter the month")))
-day =(int(input("enter the day")))
 
-if(month>=3) and (day>=20):
-  print("spring")
-elif(month>=6) and(day>=21):
-  print("summer")
-elif(month>=9) and (day>=22):
-  print("fall")
-else:
-  print("winter")
+month = input("Input the month (e.g. January, February etc.): ")
+day = int(input("Input the day: "))
+if month in ('January', 'February', 'March'):
+  season = 'winter'
+elif 
+  month in ('April', 'May', 'June'): 
+   season = 'spring'
+elif
+   month in ('July', 'August', 'September'): 
+   season = 'summer'else: 
+   season = 'autumn'
+if (month == 'March') and (day > 19):
+      season = 'spring'
+    elif
+    (month == 'June') and (day > 20):
+      season = 'summer'
+    elif 
+    (month == 'September') and (day > 21): 
+     season = 'autumn'
+    elif (month == 'December') and (day > 20): 
+       season = 'winter'print("Season is",season)
+
