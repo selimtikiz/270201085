d879a5d54f022e2280964ee2b1ff0eb77551c3f3
Selim TIKIZ <selimtikiz@std.iyte.edu.tr>
2020-11-17 12:14:43 +0000

    fibonacci

---
+++ lab5/fibonacci.py
@@ -0,0 +1,15 @@
+num1=0
+num2=1
+sum_of_num=0
+
+lenght=int(input("enter the lenght of numbers"))
+for i in range(lenght):
+  print(sum_of_num)
+  num1=num2
+  num2=sum_of_num
+  sum_of_num=num1+num2
+
+
+  
+
+  
