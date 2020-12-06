4795a204553e313a027476cd874b24a4e49a215e
Selim TIKIZ <selimtikiz@std.iyte.edu.tr>
2020-12-06 17:19:30 +0000

    re code

--- lab6/average_grades.py
+++ lab6/average_grades.py
@@ -1,37 +1,15 @@
-grade_list = []
-
-
-
-n_st = int(input("Please enter number of students:"))
-
-
-
-for i in range(n_st):
-
-    print("Student ", str(i+1))
-
-    mid_1 = int(input("Midterm 1:"))
-
-    mid_2 = int(input("Midterm 2:"))
-
-    f_grade = int(input("Final:"))
-
-grade_list.append([mid_1,mid_2,f_grade])
-
-
-
-print(grade_list)
-
-
-
-avg_grades = []
-
-
-
-for sub_grades in grade_list:
-
-    avg_grades.append(sub_grades[0]*0.3 + sub_grades[1]*0.3 + sub_grades[2]*0.4)
-
-
-
-print(avg_grades)
+grade_lst=[]
+number_of_students=int(input("enter the number of students"))
+i = 0
+while i < number_of_students:
+    print("Student :",str(i+1))
+    mid_1 = int(input("midterm1"))
+    mid_2 = int(input("midterm2"))
+    final = int(input("final"))
+    i += 1
+    grade_lst.append([mid_1,mid_2,final])
+print(grade_lst)
+average_grade_lst = []
+for sub_grades in grade_lst:
+    average_grade_lst.append(sub_grades[0]*0.3+sub_grades[1]*0.3+sub_grades[2]*0.4)
+print(average_grade_lst)
