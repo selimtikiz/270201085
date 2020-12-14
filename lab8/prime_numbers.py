1fcb3cd840299b254b5b3811a8bfa0b3f9cf26f4
Selim TIKIZ <selimtikiz@std.iyte.edu.tr>
2020-12-14 08:56:37 +0000

    prime_numbers

---
+++ lab8/prime_numbers.py
@@ -0,0 +1,15 @@
+def is_prime(n):
+    if n<=1:
+        return  False
+    for x in range(2,n):
+        if n % x == 0:
+            return False
+    return True
+
+def print_primes_between(n,m):
+    for x in range(n,m):
+        if is_prime(x):
+            print(x)
+first = int(input("Enter the first number"))
+last = int(input(("Enter the last number")))
+print_primes_between((first,last))
